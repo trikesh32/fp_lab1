357988999d19f203d19df0d3eebd4f47835db85e
Mikhail Trikashnyi <trikesh@10.211.57.87-vpn.dhcp.yndx.net>
2025-11-22 13:41:42 +0300

    f fix

--- src/Euler4.hs
+++ src/Euler4.hs
@@ -1,5 +1,7 @@
 module Euler4 where
-import Data.List (foldl')  
+
+import Data.List (foldl')
+
 
 minNum :: Int
 minNum = 100
@@ -13,44 +15,49 @@ largestPalindromeTailRec :: Int
 largestPalindromeTailRec = go maxNum 0
   where
     go i best
-      | i < minNum = best
-      | otherwise = go (i - 1) $ findPalindrome i maxNum best
-    
+        | i < minNum = best
+        | otherwise = go (i - 1) $ findPalindrome i maxNum best
+
     findPalindrome i j currentBest
-      | j < i = currentBest
-      | j < minNum = currentBest
-      | product > currentBest && isPalindrome product = findPalindrome i (j - 1) product
-      | otherwise = findPalindrome i (j - 1) currentBest
-      where product = i * j
-  
+        | j < i = currentBest
+        | j < minNum = currentBest
+        | product > currentBest && isPalindrome product = findPalindrome i (j - 1) product
+        | otherwise = findPalindrome i (j - 1) currentBest
+      where
+        product = i * j
+
 
 largestPalindromeRec :: Int
 largestPalindromeRec = search maxNum maxNum
   where
     search i j
-      | i < minNum = 0
-      | j < minNum = search (i - 1) maxNum
-      | isPalindrome product = max product (search i (j - 1))
-      | otherwise = search i (j - 1)
-      where product = i * j
+        | i < minNum = 0
+        | j < minNum = search (i - 1) maxNum
+        | isPalindrome product = max product (search i (j - 1))
+        | otherwise = search i (j - 1)
+      where
+        product = i * j
 
 
 largestPalindromeModular :: Int
-largestPalindromeModular = 
-  let
-      allProducts = [i * j | i <- [minNum..maxNum], j <- [i..maxNum]]
-      palindromes = filter isPalindrome allProducts
-  in foldl' max 0 palindromes
+largestPalindromeModular =
+    let
+        allProducts = [i * j | i <- [minNum .. maxNum], j <- [i .. maxNum]]
+        palindromes = filter isPalindrome allProducts
+     in
+        foldl' max 0 palindromes
 
 
 largestPalindromeMap :: Int
-largestPalindromeMap = 
-  maximum $ map processRow [minNum..maxNum]
+largestPalindromeMap =
+    maximum $ map processRow [minNum .. maxNum]
   where
-    processRow i = maximum $ 
-      (0 :) $ map (i *) $ 
-      filter (\j -> isPalindrome (i * j)) [i..maxNum]
+    processRow i =
+        maximum $
+            (0 :) $
+                map (i *) $
+                    filter (\j -> isPalindrome (i * j)) [i .. maxNum]
 
 
 largestPalindromeList :: Int
-largestPalindromeList = maximum [r | i <- [minNum..maxNum], j <- [i..maxNum], let r = i * j, isPalindrome r]
+largestPalindromeList = maximum [r | i <- [minNum .. maxNum], j <- [i .. maxNum], let r = i * j, isPalindrome r]
--- src/test_euler.hs
+++ src/test_euler.hs
@@ -1,28 +1,30 @@
 module Main where
 
-import Euler4
 import Euler27
+import Euler4
 import System.Exit (exitFailure, exitSuccess)
 
 testEuler4 :: IO Bool
 testEuler4 = do
     putStrLn "Testing Euler4 implementations..."
-    
+
     let expected = 906609
-    let results = [ largestPalindromeTailRec
-                  , largestPalindromeRec
-                  , largestPalindromeModular
-                  , largestPalindromeMap
-                  , largestPalindromeList
-                  ]
-    
+    let results =
+            [ largestPalindromeTailRec
+            , largestPalindromeRec
+            , largestPalindromeModular
+            , largestPalindromeMap
+            , largestPalindromeList
+            ]
+
     let allCorrect = all (== expected) results
-    
+
     if allCorrect
         then putStrLn ("All Euler4 implementations return " ++ show expected) >> return True
         else do
             putStrLn "Euler4 implementations disagree:"
-            mapM_ (\(name, result) -> putStrLn $ "  " ++ name ++ ": " ++ show result) 
+            mapM_
+                (\(name, result) -> putStrLn $ "  " ++ name ++ ": " ++ show result)
                 [ ("TailRec", largestPalindromeTailRec)
                 , ("Rec", largestPalindromeRec)
                 , ("Modular", largestPalindromeModular)
@@ -34,22 +36,24 @@ testEuler4 = do
 testEuler27 :: IO Bool
 testEuler27 = do
     putStrLn "Testing Euler27 implementations..."
-    
+
     let expected = -59231
-    let results = [ solutionTailRec
-                  , solutionRec
-                  , solutionModular
-                  , solutionMap
-                  , solutionList
-                  ]
-    
+    let results =
+            [ solutionTailRec
+            , solutionRec
+            , solutionModular
+            , solutionMap
+            , solutionList
+            ]
+
     let allCorrect = all (== expected) results
-    
+
     if allCorrect
         then putStrLn ("All Euler27 implementations return " ++ show expected) >> return True
         else do
             putStrLn "Euler27 implementations disagree:"
-            mapM_ (\(name, result) -> putStrLn $ "  " ++ name ++ ": " ++ show result) 
+            mapM_
+                (\(name, result) -> putStrLn $ "  " ++ name ++ ": " ++ show result)
                 [ ("TailRec", solutionTailRec)
                 , ("Rec", solutionRec)
                 , ("Modular", solutionModular)
@@ -63,7 +67,7 @@ main = do
     success1 <- testEuler4
     putStrLn ""
     success2 <- testEuler27
-    
+
     if success1 && success2
         then putStrLn "All tests passed!" >> exitSuccess
         else putStrLn "Some tests failed!" >> exitFailure
